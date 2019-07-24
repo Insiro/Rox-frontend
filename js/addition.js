301956a50d89f83bb1dbb78b528b5fdde7353d19
PieSignal <insiro@naver.com>
2019-07-24 23:56:10 +0900

    .

--- js/addition.js
+++ js/addition.js
@@ -7,17 +7,43 @@ function go_table() {
         location.href = "Tables.html?table=" + arguments[0];
     }
 }
-
+function go_detail(){
+    if (arguments.length < 1) {
+        console.log("None");
+        location.href = "detail.html?id=" + "None";
+    } else {
+        console.log(arguments[0]);
+        location.href = "detail.html?id=" + arguments[0];
+    }
+}
 function getGameList(theGame, length) {
     var req = new XMLHttpRequest()
     req.open("GET", "http://localhost:8080/api/list/" + theGame + "/" + length);
-    req.addEventListener("load", function() {
+    req.addEventListener("load", function () {
         if (req.status === 200) {
             console.log("GetList Get success!");
             var json = JSON.parse(req.responseText);
             console.log(json);
             var data = json.data;
             writeList(theGame, data);
+        } else {
+            writeList();
+            console.error("NONE");
+        }
+    });
+    req.send(null);
+}
+
+function get_detail(id) {
+    var req = new XMLHttpRequest()
+    req.open("GET", "http://localhost:8080/api/info/" + id);
+    req.addEventListener("load", function () {
+        if (req.status === 200) {
+            console.log("GetList Get success!");
+            var json = JSON.parse(req.responseText);
+            console.log(json);
+            var data = json.data;
+            writedata(data);
         } else {
             writeList();
             console.error("NONE");
