a4e6591368b34a1a0322afd923a320a1dbb1f3a8
piesignal <insiro@naver.com>
2019-09-03 18:07:09 +0900

    add: basic error return structure

--- php/signUp.php
+++ php/signUp.php
@@ -5,13 +5,21 @@ $pwd = password_hash($_POST['pwd']);
 $db = new mysqli($dbcred['host'], $dbcred['user']. $dbcred['pass'], $dbcred['dbname']);
 $res = $db->query("SELECT * FROM users WHERE id='".$db->real_escape_string($id)."'");
 if ($res->num_rows > 0) {
-    //THERE ARE
+    // GIT 으로 마저 작업 함.
 
-    // GIT 으로 마저 작업함.
+    die(json_encode(array("success" => false, "message" => "Following ID Already Exists!")));
 }
 else {
 
-    $db->query("INSERT INTO (id, pass) VALUES (".$db->real_escape_string($id).",".$pwd.")");
+    $res = $db->query("INSERT INTO (id, pass) VALUES (".$db->real_escape_string($id).",".$pwd.")");
+    if ($res) {
+        // DB Connection Success
+        echo json_encode(array("success" => true));
+    } else {
+        // EPIC FAIL
+        die(json_encode(array("success" => false, "message" => "Following ID Already Exists!")));
+
+    }
 }
 
 ?>
